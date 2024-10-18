9a0e65424eff4c7daa895cb94976bbf1b66ce835
Rohit Badi <157134069+Rohit742004@users.noreply.github.com>
2024-10-18 09:45:26 +0530

    Create Q9.py

---
+++ Loops/for loop/Q9.py
@@ -0,0 +1,10 @@
+#Q9.Write a python script to count total even number in the given range using for loop
+
+flag = 0
+for i in range(1,11):
+    if i%2==0 :
+        flag= flag + 1
+print("Number of even valus from 1 to 10:",flag)
+
+Output:
+Number of even valus from 1 to 10: 5
