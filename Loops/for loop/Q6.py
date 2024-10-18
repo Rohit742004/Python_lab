1bd878105d1bcce668c7036afd988cd1afa18029
Rohit Badi <157134069+Rohit742004@users.noreply.github.com>
2024-10-18 09:11:23 +0530

    Update Q6.py

--- Loops/for loop/Q6.py
+++ Loops/for loop/Q6.py
@@ -1,1 +1,20 @@
 # Q6. Write a python script to print character using for loop.
+
+strValue = input("Enter the string")
+for n in range(0,len(strValue)):
+    print(strValue[n])
+
+Output:
+Enter the string ROHIT BADI
+
+R
+O
+H
+I
+T
+ 
+B
+A
+D
+I
+
