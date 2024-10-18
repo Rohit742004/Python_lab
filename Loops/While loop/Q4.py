095f7066a414d1b531f32180e367c0af8c32a0e3
Rohit Badi <157134069+Rohit742004@users.noreply.github.com>
2024-10-18 08:52:42 +0530

    Create Q4.py

---
+++ Loops/While loop/Q4.py
@@ -0,0 +1,16 @@
+# Q4.Write a python script to print natural numbers for the given max N value using while loop.
+
+max = int(input("Enter the number")) #Q4.printing natural number for given max N value using while loop
+n=1
+while n<=max:
+    print(n)
+    n = n+1
+
+Output:
+Enter the number 5
+
+1
+2
+3
+4
+5
