18332c4e1ba61225e6634ea224e960e2f1d34a57
Rohit Badi <157134069+Rohit742004@users.noreply.github.com>
2024-10-18 08:54:55 +0530

    Create Q5.py

---
+++ Loops/While loop/Q5.py
@@ -0,0 +1,15 @@
+# Q5.Write a python script to print characters by specifying the index value using while loop
+
+string = input("Enter a String value : ") #Q5.Printing characters by specifying the index value using while loop
+i=0
+while i < len(string):
+    print(string[i])
+    i = i + 1
+
+Output: 
+Enter a String value :  badi
+
+b
+a
+d
+i
